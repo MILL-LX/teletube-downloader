a14beebdd3c9e0650f1ab2191f4f1fc6cb7ce6ba
Mo Mobarak <mo@mobarak.com>
2026-06-02 20:19:26 +0100

    include title in video filename

--- app/video_downloads.py
+++ app/video_downloads.py
@@ -9,13 +9,16 @@ YOUTUBE_URL_TEMPLATE = 'https://www.youtube.com/watch?v={video_id}'
 
 
 def _get_video_items(metadata):
-    '''Extract video ID and publish year from a metadata response.'''
+    '''Extract video ID, title, and publish year from a metadata response.'''
     items = []
     for item in metadata.get('response', []):
         video_id = item['contentDetails']['videoId']
         published_at = item['contentDetails'].get('videoPublishedAt', '')
         year = published_at[:4] if published_at else 'unknown'
-        items.append((video_id, year))
+        title = item['snippet'].get('title', video_id)
+        # Sanitize title for use in a filename
+        safe_title = ''.join(c if c.isalnum() or c in ' ._-' else '_' for c in title).strip()
+        items.append((video_id, year, safe_title))
     return items
 
 
@@ -47,13 +50,13 @@ def download_videos(metadata, data_directory):
     video_items = _get_video_items(metadata)
     logger.info(f'Found {len(video_items)} videos in metadata')
 
-    for video_id, year in video_items:
+    for video_id, year, safe_title in video_items:
         if _is_already_downloaded(video_id, ready_dir):
             logger.info(f'Skipping already downloaded video: {video_id}')
             continue
 
         url = YOUTUBE_URL_TEMPLATE.format(video_id=video_id)
-        output_template = os.path.join(in_progress_dir, f'{video_id}.%(ext)s')
+        output_template = os.path.join(in_progress_dir, f'{safe_title}-{video_id}.%(ext)s')
 
         ydl_opts = {
             'outtmpl': output_template,
@@ -76,7 +79,7 @@ def download_videos(metadata, data_directory):
             year_dir = os.path.join(ready_dir, year)
             os.makedirs(year_dir, exist_ok=True)
             for filename in os.listdir(in_progress_dir):
-                if filename.startswith(video_id):
+                if f'-{video_id}.mp4' in filename:
                     src = os.path.join(in_progress_dir, filename)
                     dst = os.path.join(year_dir, filename)
                     shutil.move(src, dst)
